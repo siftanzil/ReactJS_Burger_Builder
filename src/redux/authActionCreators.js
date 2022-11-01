52723daf3e3e0dd99b190d0bbc6dcb473c7713aa
Sifat Ullah Tanzil <siftanzil@gmail.com>
2022-11-01 10:45:20 +0600

    Final

--- src/redux/authActionCreators.js
+++ src/redux/authActionCreators.js
@@ -39,7 +39,7 @@ export const auth = (email, password, mode) => (dispatch) => {
         authUrl =
             "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
     }
-    const API_KEY = "AIzaSyBrw3kmHvjcQx-vn_hwX_D1vyWTMIeV6Ms";
+    const API_KEY = "YOUR_API_KEY";
     axios
         .post(authUrl + API_KEY, authData)
         .then((response) => {
